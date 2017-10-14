9113656bbbbf562d5500333c708c02c7d9df6f62
Fabian Fränz <f.fraenz@t-online.de>
2017-10-14 17:53:37 +0200

    5 Add license

--- src/main/kotlin/com/zypus/eliza/dsl/ScriptDSL.kt
+++ src/main/kotlin/com/zypus/eliza/dsl/ScriptDSL.kt
@@ -1,51 +1,56 @@
-package com.zypus.eliza
+package com.zypus.eliza.dsl
 
 /**
- * Data structures and dsl to create ELIZA scripts.
- * TODO seperate dsl and final data structures
+ * DSL to easily create ELIZA scripts.
  *
- * @author Fabian Fraenz <f.fraenz@t-online.de>
+ * @author Zypus
  *
- * @created 10.10.17
+ * @created 14.10.17
  */
 
+@DslMarker
+annotation class ElizaMarker
+
+@ElizaMarker
 data class Script(
 		var initial: String = "",
 		var final: String = "",
-		val quitList: MutableList<String> = arrayListOf(),
-		val preMap: MutableMap<String, String> = hashMapOf(),
-		val postMap: MutableMap<String, String> = hashMapOf(),
-		val synonymMap: MutableMap<String, Synonym> = hashMapOf(),
-		val keyMap: MutableMap<String, Key> = hashMapOf()
+		val quits: MutableList<String> = arrayListOf(),
+		val preTransformations: MutableMap<String, String> = hashMapOf(),
+		val postTransformations: MutableMap<String, String> = hashMapOf(),
+		val synonyms: MutableMap<String, Synonym> = hashMapOf(),
+		val keys: MutableMap<String, Key> = hashMapOf()
 ) {
 
-	fun onQuit(vararg messages: String) = quitList.addAll(messages)
+	fun onQuit(vararg messages: String) = quits.addAll(messages)
 
-	fun pre(vararg transformations: Pair<String, String>) = preMap.putAll(transformations)
+	fun pre(vararg transformations: Pair<String, String>) = preTransformations.putAll(transformations)
 
-	fun post(vararg transformations: Pair<String, String>) = postMap.putAll(transformations)
+	fun post(vararg transformations: Pair<String, String>) = postTransformations.putAll(transformations)
 
-	fun synon(group: String, vararg synonyms: String): Synonym {
+	fun synonym(group: String, vararg synonyms: String): Synonym {
 		val synonym = Synonym(group)
 		synonym.synonyms.add(group)
 		synonym.synonyms.addAll(synonyms)
-		synonymMap[group] = synonym
+		this.synonyms[group] = synonym
 		return synonym
 	}
 
 	fun key(name: String, weight: Int = 1, block: Key.() -> Unit) {
 		val k = Key(name, weight)
 		k.block()
-		keyMap[name] = k
+		keys[name] = k
 	}
 }
 
+@ElizaMarker
 data class Synonym(val group: String, val synonyms: MutableList<String> = arrayListOf()) {
 	override fun toString(): String {
 		return synonyms.joinToString(separator = "|", prefix = "(?:", postfix = ")")
 	}
 }
 
+@ElizaMarker
 data class Key(val name: String, val weight: Int, private val decompositions: MutableList<Decomposition> = arrayListOf(), val index: Int = nextIndex) {
 
 	companion object {
@@ -67,16 +72,17 @@ data class Key(val name: String, val weight: Int, private val decompositions: Mu
 
 	fun decompose(string: String, memorising: Boolean = false, block: Decomposition.() -> Unit) = decompose(string.toRegex(), memorising, block)
 
-	fun match(input: String): Decomposition? {
+	internal fun match(input: String): Decomposition? {
 		return decompositions.firstOrNull { dec ->
 			dec.pattern.matches(input)
 		}
 	}
 }
 
+@ElizaMarker
 data class Decomposition(val pattern: Regex, val isMemorising: Boolean = false, private val actions: MutableList<Action> = arrayListOf()) {
 
-	var counter = 0
+	private var counter = 0
 
 	fun assemble(string: String) {
 		actions += Reassembly(string)
@@ -86,7 +92,7 @@ data class Decomposition(val pattern: Regex, val isMemorising: Boolean = false, 
 		actions += Goto(target)
 	}
 
-	fun nextAction(): Action {
+	internal fun nextAction(): Action {
 		val action = actions[counter]
 		counter = (counter + 1) % actions.size
 		return action
@@ -94,10 +100,11 @@ data class Decomposition(val pattern: Regex, val isMemorising: Boolean = false, 
 
 }
 
+@ElizaMarker
 sealed class Action
 
 data class Reassembly(private val pattern: String) : Action() {
-	fun assemble(decompositionPattern: Regex, input: String, postMap: MutableMap<String, String>): String {
+	internal fun assemble(decompositionPattern: Regex, input: String, postMap: MutableMap<String, String>): String {
 		return input.replace(decompositionPattern) { result ->
 			pattern.replace("\\$\\d".toRegex()) { placeholder ->
 				val index = placeholder.value.drop(1).toInt()
